841dccdb06730944d66181b73d8785d59f01e77e
pirsonxyz <pirson@pirson.xyz>
2025-05-07 23:00:42 -0400

    count rows and total readings script
    little util to count the elements in db

---
+++ utils/count_rows.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@libsql/client";
+
+const con = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+const rows_n = await con.execute("select count(*) from readings");
+const rows = parseInt(rows_n.rows[0]["0"])
+
+console.log(`🛫 There are ${rows} rows and ${rows * 5} readings!`)
